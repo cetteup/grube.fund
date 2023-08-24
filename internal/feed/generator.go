6cb8c142fd836636958aaf7f2711fcf3905cfe52
cetteup <me@cetteup.com>
2023-08-24 21:58:27 +0200

    feat: Increase feed generator page size

--- internal/feed/generator.go
+++ internal/feed/generator.go
@@ -19,7 +19,7 @@ import (
 const (
 	timeout = 5 * time.Second
 
-	perPage   = 64
+	perPage   = 86
 	userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 )
 
